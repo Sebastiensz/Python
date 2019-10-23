87d17867aa5def1ebe4b0558de70ee5c2e813671
Sebastiensz <34402571+Sebastiensz@users.noreply.github.com>
2019-10-23 17:11:10 +0200

    Delete test.py

--- test.py
+++
@@ -1,3 +0,0 @@
-object_point_lyr.edit_features(adds = [feat])
-query_result1 = object_point_lyr.query(where='OBJECTID={0}'.format(1+frame_count//10))
-object_point_lyr.attachments.add(query_result1.features[0].attributes['OBJECTID'],'C:\\path\\fissures_detectees.jpg')

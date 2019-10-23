209c8438f51d6f52fb73ce2e821b411b1d8ca500
Sebastiensz <34402571+Sebastiensz@users.noreply.github.com>
2019-10-23 16:18:45 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-feat.attributes={'LongitudinalCrackCount' : lon_cracks_count,'LateralCrackCount' : lat_cracks_count,'AlligatorCrackCount' : allig_cracks_count,'OtherCorruptionCount' : othcorr_count}
+object_point_lyr.edit_features(adds = [feat])
+query_result1 = object_point_lyr.query(where='OBJECTID={0}'.format(1+frame_count//10))
+object_point_lyr.attachments.add(query_result1.features[0].attributes['OBJECTID'],'C:\\path\\fissures_detectees.jpg')

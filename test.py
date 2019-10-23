8237397d715ba1f36c42e50cc79318e1bde28db4
Sebastiensz <34402571+Sebastiensz@users.noreply.github.com>
2019-10-23 16:11:11 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-points_item = gis1.content.search("Nom_de_votre_couche_dentites", item_type="Feature Service")[0]
+detection_graph = tf.Graph()
+
+with detection_graph.as_default():
+
+    od_graph_def = tf.GraphDef()
+
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+
+        serialized_graph = fid.read()
+
+        od_graph_def.ParseFromString(serialized_graph)
+
+        tf.import_graph_def(od_graph_def, name='')

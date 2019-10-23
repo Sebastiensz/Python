c448714d2c8899cfda3ec3c7000c6c7d3101c552
Sebastiensz <34402571+Sebastiensz@users.noreply.github.com>
2019-10-23 16:09:10 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,24 @@
-from arcgis.gis import GIS
-gis1 = GIS('https://urldevotreportail.com, "votre_username", votremotdepasse)
+import arcgis
+import arcpy
+import moviepy.editor as mpy
+import datetime
+import os
+import glob
+import copy
+from copy import deepcopy
+import numpy as np
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+import cv2
+import time
+from object_detection.utils import label_map_util #package local
+from object_detection.utils import visualization_utils as vis_util #package local
+import scipy.misc
+import pandas as pd
+import IPython.display
